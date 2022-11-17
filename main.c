36a271d9c46a9922bfe7c73a0d87b4b274540240
PkHila <105527173+PkHila@users.noreply.github.com>
2022-11-16 22:01:35 -0300

    Funcion textoAString + Archivo de texto de prueba
    Implementa la funcion para crear una string dinamica y pasar la totalidad de un texto a una string gigante.

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+
+  char palabra[20];
+  int idDOC;
+  int pos; //incrementa palabra por palabra, y no letra por letra
+
+}termino;
+
+
+
+void inicializarString(char**miString, long longitud)
+{
+    *miString = (char*)malloc(sizeof(longitud));
+}
+
+void cargarTextoAString(char* nombreTexto, char** stringTexto)
+{
+    long cant;
+    FILE* arch = fopen(nombreTexto,"rb");
+    if(arch)
+    {
+        fseek(arch, 0, SEEK_END);
+        cant = ftell(arch);
+        printf("el archivo tiene %ld bytes\n", cant);
+        rewind(arch);
+        inicializarString(stringTexto, cant+1);
+        fread(*stringTexto, sizeof(char),cant,arch);
+        printf("%s",*stringTexto);
+
+
+    }
+    fclose(arch);
+}
+
 
 int main()
 {
-    printf("Hello world!\n");
+    char* stringTotal;
+    cargarTextoAString("Prueba.txt", &stringTotal);
+    printf("%s", stringTotal);
     return 0;
 }
